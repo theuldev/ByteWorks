bb6166370637744ccc9b82a1bf8f7e81e3b3bb6e
lstheus <matheus18sdl@outlook.com>
2024-03-24 13:39:08 -0300

    fix: allow save current theme if user reload in localStorage

--- src/shared/services/ThemeService.ts
+++ src/shared/services/ThemeService.ts
@@ -1,20 +1,29 @@
 // theme.service.ts
-import { Injectable } from '@angular/core';
+import { isPlatformBrowser, isPlatformServer } from '@angular/common';
+import { Inject, Injectable, PLATFORM_ID } from '@angular/core';
 import { BehaviorSubject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ThemeService {
-    private darkModeSubject = new BehaviorSubject<boolean>(false);
+     private isLocalStorageAvailable = typeof localStorage !== 'undefined';
+    private darkModeSubject = new BehaviorSubject<boolean>( this.isLocalStorageAvailable ? JSON.parse(localStorage.getItem('theme')!) : false);
     darkMode$ = this.darkModeSubject.asObservable();
-  
-    constructor() {}
+ 
+    constructor(
+      @Inject(PLATFORM_ID) private platformId: Object) {
+        
+      }
+
+
   
     toggleDarkMode() {
       
       this.darkModeSubject.next(!this.darkModeSubject.value);
-    }
+      localStorage.setItem('theme', this.darkModeSubject.value.toString())
+
+ }
   
     isDarkMode(): boolean {
       return this.darkModeSubject.value;
