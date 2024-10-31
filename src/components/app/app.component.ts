f841093b3ca49ecf32a11fe3b987a596a4e02b4e
theul <matheus18sdl@outlook.com>
2024-10-31 11:11:38 -0300

    fix: update footer and other elements

--- src/components/app/app.component.ts
+++ src/components/app/app.component.ts
@@ -2,7 +2,9 @@ import { Component, Inject, PLATFORM_ID } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ThemeService } from '../../shared/services/theme.service';
 import { isPlatformBrowser, isPlatformServer } from '@angular/common';
-
+import { fromEvent } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { toSignal } from '@angular/core/rxjs-interop';
 @Component({
   selector: 'bw-root',
   standalone: true,
@@ -16,8 +18,17 @@ export class AppComponent {
   constructor(private themeService: ThemeService, @Inject(PLATFORM_ID) private platformId: Object) {
     if (!isPlatformServer(this.platformId)) {
       this.applyTheme();
+      // this.getMouseCoordinates();
     }
   }
+  // getMouseCoordinates(){
+  //     var source = fromEvent(document, 'mousemove');
+  //     var subscription = source.subscribe(e => 
+  //     {
+  //       console.log(e)
+        
+  //     });
+  // }
 
   applyTheme(): void {
     const darkMode = this.themeService.isDarkMode();
